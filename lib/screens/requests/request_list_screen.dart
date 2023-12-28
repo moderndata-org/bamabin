df44573a7abf0feeaaef2828132c87e4505add86
Fatemeh <fathifatemeh782@gmail.com>
2023-12-28 06:04:08 -0800

    Film request dialog

--- lib/screens/requests/request_list_screen.dart
+++ lib/screens/requests/request_list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bamabin/constant/colors.dart';
 import 'package:bamabin/widgets/details_app_bar.dart';
+import 'package:bamabin/widgets/dialog_items/film_request_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,49 +14,63 @@ class RequestListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          backgroundColor: cPrimary,
-          body: Container(
-            width: Get.width,
-            height: Get.height,
-            child: Column(
-              children: [
-                DetailsAppBar(
-                    leftWidget: MyTextButton(
-                        size: const Size(40, 40),
-                        onTap: () {
-                          Get.back();
-                        },
-                        fgColor: cGrey,
-                        bgColor: cPrimaryDark,
-                        child: Icon(
-                          Icons.arrow_back_ios,
-                          color: cGrey,
-                        )),
-                    rightWidget: MyTextButton(
-                        size: const Size(40, 40),
-                        onTap: () {
-                          Get.back();
-                        },
-                        fgColor: cGrey,
-                        bgColor: cPrimaryDark,
-                        child: Icon(
-                          Icons.video_call,
-                          color: cAccent,
-                        )),
-                    title: "درخواست فیلم و سریال"),
-                Expanded(child:    ListView(
-                  padding: EdgeInsets.only(right: 10,left: 10),
-
-                  children: [
-                    RequestItem(),
-                    RequestItem()
-
-
-                  ],
-                ))
-              ],
+      backgroundColor: cPrimary,
+      body: Container(
+        width: Get.width,
+        height: Get.height,
+        child: Column(
+          children: [
+            DetailsAppBar(
+                leftWidget: MyTextButton(
+                    size: const Size(40, 40),
+                    onTap: () {
+                      Get.back();
+                    },
+                    fgColor: cGrey,
+                    bgColor: cPrimaryDark,
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: cGrey,
+                    )),
+                rightWidget: MyTextButton(
+                    size: const Size(40, 40),
+                    onTap: () {
+                      Get.back();
+                    },
+                    fgColor: cGrey,
+                    bgColor: cPrimaryDark,
+                    child: Icon(
+                      Icons.video_call,
+                      color: cAccent,
+                    )),
+                title: "درخواست فیلم و سریال"),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.only(right: 10, left: 10),
+                children: [RequestItem(), RequestItem()],
+              ),
             ),
-          ),
-        ));
+         Container(
+           padding: EdgeInsets.all(10),
+           child:
+           MyTextButton(
+             size: Size(Get.width, 45),
+             bgColor: cAccent,
+             child: Text(
+               "درخواست جدید",
+               style: TextStyle(
+                   color: Colors.black, fontWeight: FontWeight.bold,fontSize: 15),
+             ),
+             onTap: () {
+               showDialog(context: context, builder: (context) {
+              return   FilmRequestDialog();
+               },);
+             },
+           ),
+         )
+          ],
+        ),
+      ),
+    ));
   }
 }
