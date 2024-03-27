d92d402d4e232693ab28780db8e816913997ee43
Mohamad Mohamadi <mohamadmohamadi249@gmail.com>
2024-03-27 16:50:21 +0330

    player

--- lib/screens/player/player_screen.dart
+++ lib/screens/player/player_screen.dart
@@ -380,6 +380,7 @@ class _PlayerScreenState extends State<PlayerScreen> {
                                                           )),
 
 
+
                                                       Visibility(
                                                         visible: controller
                                                             .is_dubbed.isFalse,
